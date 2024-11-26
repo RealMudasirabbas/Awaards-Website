0a6b33849f33e36ef86239026539f782478809a5
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-11-26 17:13:25 +0500

    Hero Section Completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+  <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <Hero />
+    <section className="min-h-screen bg-blue-500"></section>
+  </main>
+  )
+}
+
+export default App
